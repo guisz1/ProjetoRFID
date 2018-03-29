b9c8e6f8493085f9aa0c51c9cdbe5431713ddf57
guilherme.sz <lasse@guilherme.sz@pti.org.br>
2018-03-29 09:14:31 -0300

    Segundo commit

--- DAO/listaPresensaDAO.php
+++ DAO/listaPresensaDAO.php
@@ -17,7 +17,7 @@
       		return $dados;
     	}
     	function buscaUltimo(){
-    		$query = "SELECT tbUsuario.nomeUsuario,tbEvento.nomeEvento, listaPresensa.status, listaPresensa.horario FROM ((listaPresensa INNER JOIN tbUsuario ON listaPresensa.idUsuario = tbUsuario.idUsuario) INNER JOIN tbEvento ON listaPresensa.idEvento = tbEvento.idEvento)";
+    		$query = "SELECT tbUsuario.nomeUsuario,tbEvento.nomeEvento, listaPresensa.status, listaPresensa.horario FROM ((listaPresensa INNER JOIN tbUsuario ON listaPresensa.idUsuario = tbUsuario.idUsuario) INNER JOIN tbEvento ON listaPresensa.idEvento = tbEvento.idEvento) ORDER BY horario DESC LIMIT 1";
     		$busca = $this->con->query($query);
     		$dados = $busca->fetchAll(PDO::FETCH_ASSOC);
     		return $dados
