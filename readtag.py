16e5fee11dd34fb163906a09837c1d71e07b7449
root <root@guilherme.sz@pti.org.br>
2018-06-14 10:49:42 -0300

    novo leitor arrumado

--- readtag.py
+++ readtag.py
@@ -6,21 +6,21 @@ import MFRC522
 import urllib2
 
 def main():
-	try:
-		LeitorRFID = MFRC522.MFRC522()
-		while True:
-			status, tag_type = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
-			if status == LeitorRFID.MI_OK:
-				print(status)
-            	status, uid = LeitorRFID.MFRC522_Anticoll()
-	            if status == LeitorRFID.MI_OK:
-	                uid = ''.join('%02X' % c for c in uid)
-	                print(uid)
-					conn = urllib2.httplib.HTTPConnection('localhost')
-					headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-					conn.request('POST', '/ProjetoRFID/view/recebeCracha.php', 'cartao='+uid, headers)
-	except KeyboardInterrupt:
-		GPIO.cleanup()
+    try:
+        LeitorRFID = MFRC522.MFRC522()
+        while True:
+            status, tag_type = LeitorRFID.MFRC522_Request(LeitorRFID.PICC_REQIDL)
+            if status == LeitorRFID.MI_OK:
+                print(status)
+                status, uid = LeitorRFID.MFRC522_Anticoll()
+                if status == LeitorRFID.MI_OK:
+                    uid = ''.join('%02X' % c for c in uid)
+                    print(uid)
+                    conn = urllib2.httplib.HTTPConnection('localhost')
+                    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+                    conn.request('POST', '/ProjetoRFID/view/recebeCracha.php', 'cartao='+uid, headers)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 if __name__ == '__main__':
-	main()
+    main()
